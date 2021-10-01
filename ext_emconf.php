3a3db62de809a7c2518136f7c8fc2fa81379ca99
Johannes Seipelt <johannes.seipelt@3m5.de>
2021-10-01 17:41:54 +0200

    raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'adam.koppe@3m5.de',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.1',
     'author_company' => '3m5. Media GmbH',
+    'version' => '1.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.4.99',
-            'hotspot_wizard' => '1.0.0-1.0.99'
+            'hotspot_wizard' => '1.0.0-1.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
